88a4b04159337c82ac21f00287da43dec3eb34dd
Phatthanadol <622021122@tsu.ac.th>
2022-02-05 09:55:26 +0700

    auth controller updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -32,12 +33,40 @@ class AuthController extends Controller
         return response($response, 201);
     }
 
-    public function login() {
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
 
+        //check email
+        $user = User::where('email', $fields['email'])->first();
+
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Invalid Login',
+            ], 401);
+        } else {
+            $user->tokens()->delete();
+            $token = $user->createToken($request->userAgent(), ['$user->role'])->plainTextToken;
+
+            $response = [
+                'user' => $user,
+                'token' => $token,
+            ];
+
+            return response($response, 200);
+        }
     }
 
-    public function logout() {
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
 
+        return response([
+            'message' => 'Logged Out',
+        ], 200);
     }
     /**
      * Display a listing of the resource.
